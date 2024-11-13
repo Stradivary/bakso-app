7ad43f382d37b53cc05c4b683013d16d1649eca5
Ahmad Muzaki <ramouz.muzacky@gmail.com>
2024-11-13 15:48:04 +0000

    feat: personalize every instance to handle multi tab

--- frontend/src/shared/services/supabaseService.ts
+++ frontend/src/shared/services/supabaseService.ts
@@ -4,12 +4,21 @@ import { Database } from "../models/supabase";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL ?? "https://tfqnagzgyzfspmbladso.supabase.co";
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+function getTabStorageKey() {
+  if (!window.name) {
+    window.name = Math.random().toString(36).substring(4);
+  }
+  return `supabase-auth-token-${window.name}`;
+}
+
+
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
 
   auth: {
     autoRefreshToken: true,
     persistSession: true,
     storage: sessionStorage,
+    storageKey: getTabStorageKey(),
     detectSessionInUrl: false,
 
   }
