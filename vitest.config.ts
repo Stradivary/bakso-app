a60e290935a5333d050c597bf0009fc111956403
Ahmad Muzaki <ramouz.muzacky@gmail.com>
2024-11-15 18:13:39 +0000

    ci: fix coverage configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,11 +5,14 @@ export default defineConfig({
   test: {
     coverage: {
       exclude: ["**/node_modules/**", "./src/main.tsx"],
+      reporter: ['text', 'json', 'lcov', 'html'],
+      
     },
     exclude: ["**/node_modules/**", "./src/main.tsx"],
     environment: "jsdom",
     setupFiles: "./src/tests.setup.ts",
     snapshotSerializers: ["./mantine.serializer.js"],
+    
     alias: [
       { find: "@/", replacement: "/src/" },
     ]
