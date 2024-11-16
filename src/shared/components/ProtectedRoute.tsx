9b721c2377f07313b1c6b7947fdd949cee5d58fe
Ahmad Muzaki <ramouz.muzacky@gmail.com>
2024-11-16 14:06:42 +0000

    test: improve coverage

--- src/shared/components/ProtectedRoute.tsx
+++ src/shared/components/ProtectedRoute.tsx
@@ -2,15 +2,15 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
-export const ProtectedRoute: React.FC<{ children: JSX.Element; }> = ({
+export const ProtectedRoute: React.FC<{ children: JSX.Element }> = ({
   children,
 }) => {
   const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
   useEffect(() => {
     if (!isAuthenticated) {
-      navigate('/login')
+      navigate?.("/login");
     }
-  }, [isAuthenticated, navigate])
+  }, [isAuthenticated, navigate]);
   return children;
 };
