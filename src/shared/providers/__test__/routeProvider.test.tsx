fd05264579edff93de14231525fd50b89722f0cf
Ahmad Muzaki <ramouz.muzacky@gmail.com>
2024-11-17 15:00:56 +0000

    test: fix invalid test snapshots

--- src/shared/providers/__test__/routeProvider.test.tsx
+++ src/shared/providers/__test__/routeProvider.test.tsx
@@ -8,18 +8,62 @@ import {
   it,
   vi,
 } from "vitest";
+import { AuthProvider } from "../authProvider";
 import { RouteProvider } from "../routeProvider";
+interface MockCoordinates {
+  latitude: number | null;
+  longitude: number | null;
+  accuracy: number;
+  altitude: null | number;
+  altitudeAccuracy: null | number;
+  heading: null | number;
+  speed: null | number;
+}
+
+// Default mock coordinates
+const defaultCoords: MockCoordinates = {
+  latitude: 1,
+  longitude: 1,
+  accuracy: 100,
+  altitude: null,
+  altitudeAccuracy: null,
+  heading: null,
+  speed: null,
+};
+
+// Create mock geolocation object
+const mockGeolocation = {
+  getCurrentPosition: vi.fn().mockImplementation((success) =>
+    Promise.resolve(
+      success({
+        coords: defaultCoords,
+        timestamp: Date.now(),
+      }),
+    ),
+  ),
+  watchPosition: vi.fn(),
+  clearWatch: vi.fn(),
+};
 
 class MockResizeObserver {
   observe = vi.fn();
   unobserve = vi.fn();
   disconnect = vi.fn();
 }
+vi.mock("../../../shared/components/ProtectedRoute", () => {
+  return {
+    ProtectedRoute: vi.fn(({ children }) => <div>{children}</div>),
+  };
+});
 
 describe("RouteProvider", () => {
   // Mock implementation
   beforeAll(() => {
     vi.stubGlobal("ResizeObserver", MockResizeObserver);
+    Object.defineProperty(global.navigator, "geolocation", {
+      value: mockGeolocation,
+      writable: true,
+    });
   });
 
   // Clean up after tests
@@ -32,7 +76,11 @@ describe("RouteProvider", () => {
     vi.clearAllMocks();
   });
   it("renders without crashing", () => {
-    const { container } = render(<RouteProvider />);
+    const { container } = render(
+      <AuthProvider>
+        <RouteProvider />
+      </AuthProvider>,
+    );
     expect(container).toMatchSnapshot();
   });
 });
