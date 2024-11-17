186674ccaae176fc0f3a3075555d1a6afadf7725
Ahmad Muzaki <ramouz.muzacky@gmail.com>
2024-11-17 04:21:19 +0000

    chore: update constants

--- src/shared/utils/constants.ts
+++ src/shared/utils/constants.ts
@@ -10,8 +10,6 @@ export const LOCATION_UPDATE_INTERVAL = 10 * 1000; // 30 seconds
 export const BASE_ROUTING_URL =
   "https://routing.openstreetmap.de/routed-foot/route/v1/driving";
 
-export const supabaseUrl =
-  import.meta.env.VITE_SUPABASE_URL ??
-  "https://tfqnagzgyzfspmbladso.supabase.co";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL ?? "";
 
 export const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY ?? "";
